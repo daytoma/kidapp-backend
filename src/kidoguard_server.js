3850f3a0b06021bb2ee119ab0d2f085d26e72f22
daytoma <daytoma@gmail.com>
2026-07-24 17:55:00 +0200

    Add files via upload

--- src/kidoguard_server.js
+++ src/kidoguard_server.js
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 4000;
 const JWT_SECRET = 'kidapp-super-secret-key-2026';
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
 
 // Serve static PWA frontend files
 app.use(express.static(path.join(__dirname, '../../kidoguard')));
